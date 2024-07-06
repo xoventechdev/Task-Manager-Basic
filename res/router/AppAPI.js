fe79f8293a78b7a5231b3fdbd4a43bfe043160b1
xoventechdev <alkarim.doha@gmail.com>
2024-07-06 13:36:08 +0300

    bug fixed

--- res/router/AppAPI.js
+++ res/router/AppAPI.js
@@ -10,7 +10,6 @@ import {
 } from "../controllers/user/UserController.js";
 import { verifyToken } from "../middleware/Auth.js";
 import {
-  allTaskList,
   countTaskByStatus,
   createTask,
   deleteTask,
